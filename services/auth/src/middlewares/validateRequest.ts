d43f7ffec1661bae7e97a3e3415ea4a11bea74de
bineetNaidu <bineet.s.naidu@gmail.com>
2021-08-06 21:23:35 +0530

    👾 add middlewares for validation checks

---
+++ services/auth/src/middlewares/validateRequest.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+import { validationResult } from 'express-validator';
+
+export const validateRequest = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.mapped(),
+    });
+  }
+
+  return next();
+};
