05327e1efb20851ce8f93422350baafc7e3e1212
bineetNaidu <bineet.s.naidu@gmail.com>
2021-08-12 11:50:57 +0530

    ❇️ config express session

--- services/auth/src/app.ts
+++ services/auth/src/app.ts
@@ -6,6 +6,7 @@ import dotenv from 'dotenv';
 import { apiRoutes } from './api';
 import { NotFoundError } from './utils/notFoundError';
 import { ExpressErrorHandler } from './utils/ExpressErrorHandler';
+import session from 'express-session';
 
 dotenv.config();
 const app = express();
@@ -15,6 +16,20 @@ app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'secret',
+    name: 'qid',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 30,
+      httpOnly: true,
+      sameSite: 'lax',
+      secure: false,
+    },
+  })
+);
 
 app.use('/api/auth', apiRoutes);
 
